89697b5eaefbbcb312d36cc6d8bb5a94058176d4
Altmerian <altmersc@gmail.com>
2025-05-17 21:44:50 +0200

    feat: update project configuration and add webpack setup

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,12 @@
 module.exports = {
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/'],
-  testMatch: ['**/?(*.)+(test).ts'],
-  moduleFileExtensions: ['ts', 'js', 'json'],
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/"],
+  testMatch: ["**/?(*.)+(test).ts"],
+  moduleFileExtensions: ["ts", "js", "json"],
   restoreMocks: true,
   resetMocks: true,
-  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  moduleDirectories: ["node_modules", "<rootDir>/src"],
   transform: {
-    '^.+\\.ts?$': 'ts-jest',
+    "^.+\\.ts?$": "ts-jest",
   },
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
-  // Run tests in sequence to avoid port conflicts
-  maxWorkers: 1
 };
